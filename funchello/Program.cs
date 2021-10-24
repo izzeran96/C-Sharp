d1754d7e0bf9e71c391904b069c125f3623dd2f7
Izzeran Shah <izzdotnet@gmail.com>
2021-10-25 04:06:20 +0800

    second update

--- funchello/Program.cs
+++ funchello/Program.cs
@@ -11,8 +11,17 @@ namespace funchello
         static void myFunc(){
             Console.WriteLine("Hello How Are you?");
         }
+
+        static void Calc(int arg){
+            arg += 10;
+            Console.WriteLine($"{arg}");
+
+        }
         static void Main(string[] args)
         {
+            int val = 10;
+            Calc(val);
+            Console.WriteLine($"{val}");
             PutYourName();
             Console.WriteLine("Hello World!");
             myFunc();

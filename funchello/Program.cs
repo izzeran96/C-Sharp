7f087b33af47fb2b1df7693764d5921e8ca7ca3b
Izzeran Shah <izzdotnet@gmail.com>
2021-10-25 02:50:33 +0800

    first commit

---
+++ funchello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace funchello
+{
+    class Program
+    {
+        static void PutYourName(){
+            Console.WriteLine("Put your name");
+            Console.ReadLine();
+        }
+        static void myFunc(){
+            Console.WriteLine("Hello How Are you?");
+        }
+        static void Main(string[] args)
+        {
+            PutYourName();
+            Console.WriteLine("Hello World!");
+            myFunc();
+        }
+    }
+}
